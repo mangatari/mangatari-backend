6c253165650e7aaacc06d663c6f254a654e86e2f
bebopmot <bebopmotinc@gmail.com>
2025-06-12 09:49:53 +0200

    Deploy fix 3

--- app.ts
+++ app.ts
@@ -15,7 +15,6 @@ import config from "./config";
 config(app);
 
 app.use((req,res,next) => {
-    res.header("Access-Control-Allow-Origin", "*")
     res.header("Access-Control-Allow-Methods", "*")
     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization")
     next()
